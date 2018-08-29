b4fa940f8f818f7b553473f46ed10e75138e51e3
dodziomek <dodziomek@smartadmins-macbook-pro.home>
2018-08-29 12:07:27 +0200

    add comments

--- app/Man.ts
+++ app/Man.ts
@@ -1,11 +1,33 @@
 import { Employee } from './Employee';
 
+/**
+ * Man class
+ *
+ * @export
+ * @class Man
+ * @extends {Employee}
+ */
 export class Man extends Employee {
 
+	/**
+	 * Creates an instance of Man.
+	 * 
+	 * @param {string} name
+	 * @param {number} g
+	 * @param {number} w
+	 * @param {string} p
+	 * @param {string} power
+	 * @memberof Man
+	 */
 	constructor(name: string, g: number, w: number, p: string, protected power: string) {
 		super(name, g, w, p);
 	}
 
+	/**
+	 * Main method of the Man class
+	 *
+	 * @memberof Man
+	 */
 	public hardWork() {
 		$('textarea.textarea.is-loading.is-info').append(
 `${this.name} as a Man has ${this.growth} cm growth, 
